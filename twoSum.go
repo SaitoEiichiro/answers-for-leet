6cdc11ba5e5086d60aac2bddac45b0fa6ab8e308
saitoeiichiro <saitoeiichiro@team-lab.com>
2025-01-20 18:10:51 +0900

    fix: 不要な値を削除

--- twoSum.go
+++ twoSum.go
@@ -2,8 +2,8 @@ package main
 
 func twoSum(nums []int, target int) []int {
 	ans := make([]int, 2)
-	for i, _ := range nums {
-		for k, _ := range nums {
+	for i := range nums {
+		for k := range nums {
 			if i < k {
 				if target == nums[i]+nums[k] {
 					ans[0] = i
